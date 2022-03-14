e38d905aff68613db38e0fd6e1587a087a90e80a
Fondo <Fondo@Fondo.mshome.net>
2022-03-15 00:04:07 +0200

    Commit inicial do projecto

---
+++ src/main/java/mz/gov/misau/serverIP/ServerIpApplication.java
@@ -0,0 +1,13 @@
+package mz.gov.misau.serverIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerIpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerIpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/gov/misau/serverIP/ServerIpApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.gov.misau.serverIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerIpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
